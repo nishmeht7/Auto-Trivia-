8c0778890b10e6edd628f3eed2e71d1e249d6843
Matt <matt@medean.com>
2017-04-22 00:39:21 -0500

    Added another

--- data/questionMigration.sql
+++ data/questionMigration.sql
@@ -4,4 +4,5 @@ VALUES
 (2,E'What make is this car?',E'http://media.zenfs.com/en-US/cms/autos/Boldride/subaru3601.jpg',50,E'2017-04-21 23:34:19.898-05',E'2017-04-21 23:34:19.898-05'),
 (3,E'What make is this?',E'https://upload.wikimedia.org/wikipedia/commons/6/6a/1916-apperson.jpg',50,E'2017-04-21 23:37:52.103-05',E'2017-04-21 23:37:52.103-05'),
 (4,E'What car is this?',E'https://i.kinja-img.com/gawker-media/image/upload/s--3LFGAcUh--/c_scale,fl_progressive,q_80,w_800/1360750425300314769.jpg',20,E'2017-04-21 23:54:42.077-05',E'2017-04-21 23:54:42.077-05'),
-(5,E'What car is this?',E'http://momentcar.com/images/1970-ghibli-1.jpg',25,E'2017-04-21 23:59:51.641-05',E'2017-04-21 23:59:51.641-05');
+(5,E'What car is this?',E'http://momentcar.com/images/1970-ghibli-1.jpg',25,E'2017-04-21 23:59:51.641-05',E'2017-04-21 23:59:51.641-05'),
+(6,E'What car is this?',E'https://ccpublic.blob.core.windows.net/cc-temp/listing/97/5908/7093203-1968-pontiac-firebird-400-thumb-c.jpg',15,E'2017-04-22 00:18:10.232-05',E'2017-04-22 00:18:10.232-05');
